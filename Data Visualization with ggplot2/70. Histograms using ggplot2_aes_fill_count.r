a10b26eb9c24d5dd0864dfc911d09b0f815defb9
Alison Jing Huang <alisonjhuang@gmail.com>
2019-08-11 00:02:07 -0700

    Update ggpplot 2 histograms

---
+++ Data Visualization with ggplot2/70. Histograms using ggplot2_aes_fill_count.r
@@ -0,0 +1,17 @@
+# install. pacakages('ggplot2')
+# install.packages('ggplot2movies)
+library(ggplot2)
+library(ggplot2movies)
+
+# DATA & AESTHETICS
+pl = ggplot(movies, aes(x=rating))
+
+# Geometry
+#alpha is transparency measurement
+pl2 <- pl + geom_histogram(binwidth = 0.1, color='red', fill='pink', alpha=0.4)
+
+pl3 <- pl2 + xlab('Movie Rating') + ylab('Count')
+
+print(pl3 + ggtitle("MY TITLE")) + theme_update(plot.title = element_text(hjust = 0.5))
+
+
